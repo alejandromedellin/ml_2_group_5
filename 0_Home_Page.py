d11b1610185e0b1357db301e6f864fd5f93f8136
alejandromedellin <alejandro.medellin@student.ie.edu>
2025-04-03 13:21:31 +0200

    Add files via upload

---
+++ 0_Home_Page.py
@@ -0,0 +1,24 @@
+#0. Importing the necessary libraries
+import streamlit as st
+
+# --- Streamlit UI --- #
+st.image('assets/logoparamonegro.png', width=200)
+st.markdown("---")
+st.title("🤖 Páramo GPT")  
+
+st.markdown("""### What is Páramo GPT?
+
+**Páramo GPT** is an enterprise-level **Retrieval-Augmented Generation (RAG)** application designed to enhance how the company interacts with its supplier payment data. Instead of manually searching through large PDFs, users can simply ask natural-language questions and get instant, accurate answers.""")
+
+st.markdown("""### Why is Páramo GPT valuable?
+By integrating AI with Páramo’s existing workflows, this tool delivers:
+
+- ✅ **Faster access to payment information**
+- ✅ **Reduced errors when verifying transactions**
+- ✅ **Time savings for audits, reports, and support**
+- ✅ **Scalable insights as data volume grows**
+
+In short, **Páramo GPT transforms raw financial documents into actionable insights**, boosting transparency and operational efficiency across the organization.""")
+
+st.markdown("---")
+    
